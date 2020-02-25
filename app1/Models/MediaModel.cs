1893d7213382755f637cbf1685e4a9314b19a98f
Daniel Tegelberg <tegelberg1@hotmail.com>
2020-02-25 10:34:29 +0100

    Add project files.

---
+++ app1/Models/MediaModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace app1.Models
+{
+    public class MediaModel
+    {
+        public Guid MediaID { get; set; }
+
+        public string Path { get; set; }
+
+        public DateTime LocalDateTime { get; set; }
+
+        private FileImageSource source = null;
+        public FileImageSource Source => source ?? (source = new FileImageSource() { File = Path }); 
+
+    }
+}
